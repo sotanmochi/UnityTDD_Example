8ea28707970279483a5d4f70e38e74ae8e784196
sotanmochi <stmczk@gmail.com>
2021-10-07 00:01:40 +0900

    test: Animationイベントのテストコードを追加

--- Assets/A2_AnotherCustomButton/Tests/CustomButtonContextTest.cs
+++ Assets/A2_AnotherCustomButton/Tests/CustomButtonContextTest.cs
@@ -131,4 +131,122 @@ public class CustomButtonContextTest
             Assert.That(_eventCount, Is.EqualTo(0));
         });
     }
+
+    class AnimationEventTest
+    {
+        private CustomButtonContext _context;
+        private int _eventCount;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _eventCount = 0;
+            _context = new CustomButtonContext();
+        }
+
+        [Test]
+        public void ボタンを押した瞬間にPressアニメーションが再生される()
+        {
+            _context.OnPressed += () => _eventCount++;
+
+            _context.SetButtonEnter();
+            _context.SetButtonDown();
+
+            Assert.That(_eventCount, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void ボタンを離した瞬間にReleaseアニメーションが再生される()
+        {
+            _context.OnReleased += () => _eventCount++;
+
+            _context.SetButtonEnter();
+            _context.SetButtonDown();
+            _context.SetButtonUp();
+
+            Assert.That(_eventCount, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void ボタンを押した後ボタンから指をずらした場合Releaseアニメーションが再生される()
+        {
+            _context.OnReleased += () => _eventCount++;
+
+            _context.SetButtonEnter();
+            _context.SetButtonDown();
+            _context.SetButtonExit();
+
+            Assert.That(_eventCount, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void ボタンを押した後ボタンから指をずらして再び戻ってきた場合Pressedアニメーションが再生される()
+        {
+            _context.OnPressed += () => _eventCount++;
+
+            _context.SetButtonEnter();
+            _context.SetButtonDown();
+            _context.SetButtonExit();
+            _context.SetButtonEnter();
+
+            Assert.That(_eventCount, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void ボタン以外を押した状態で指をずらした場合Pressedアニメーションが再生されない()
+        {
+            _context.OnPressed += () => _eventCount++;
+
+            _context.SetButtonEnter();
+            _context.SetButtonDown();
+            _context.SetButtonExit();
+
+            Assert.That(_eventCount, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void ボタン以外を押した状態で指をずらした場合Releasedアニメーションが再生されない()
+        {
+            _context.OnReleased += () => _eventCount++;
+
+            _context.SetButtonEnter();
+
+            Assert.That(_eventCount, Is.EqualTo(0));
+        }
+
+        [UnityTest]
+        public IEnumerator 長押しに成功したときReleaseアニメーションが再生される() => UniTask.ToCoroutine(async() =>
+        {
+            _context.OnReleased += () => _eventCount++;
+
+            var durationSeconds = 2.0f;
+            _context.SetLongPressDuration(durationSeconds);
+
+            _context.SetButtonEnter();
+            _context.SetButtonDown();
+            await UniTask.Delay(TimeSpan.FromSeconds(durationSeconds + 0.1f));
+
+            _context.SetButtonUp();
+
+            Assert.That(_eventCount, Is.EqualTo(1));
+        });
+
+        [UnityTest]
+        public IEnumerator 長押しに成功してReleaseアニメーションが再生された場合指を離すまでアニメーションが再生されない()  => UniTask.ToCoroutine(async() =>
+        {
+            _context.OnReleased += () => _eventCount++;
+
+            var durationSeconds = 2.0f;
+            _context.SetLongPressDuration(durationSeconds);
+
+            _context.SetButtonEnter();
+            _context.SetButtonDown();
+            await UniTask.Delay(TimeSpan.FromSeconds(durationSeconds + 0.1f));
+
+            _context.SetButtonExit();
+            _context.SetButtonEnter();
+
+            Assert.That(_eventCount, Is.EqualTo(0));
+        });
+    }
 }
